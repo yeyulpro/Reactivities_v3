5355ef8c98ee7ed0443e45d5b3f248679e6ceecf
Yeyul Choi <yeyulchoi2@gmail.com>
2025-08-12 00:16:30 -0400

    final wrapup

--- Application/Activities/Queries/ActivityParams.cs
+++ Application/Activities/Queries/ActivityParams.cs
@@ -9,6 +9,6 @@ namespace Application.Activities.Queries
     public class ActivityParams : PaginationParams<DateTime?>
     {
         public string? Filter { get; set; }
-        public DateTime StartDate { get; set; } = DateTime.UtcNow;
+        public DateTime? StartDate { get; set; } = DateTime.UtcNow;
     }
 }
